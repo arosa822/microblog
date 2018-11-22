2b8ad63c02fc7bc85600461b6bec2ccacd5184cc
Alex Rosa <h.rosa822@gmail.com>
2018-11-21 18:32:20 -0700

    setup for registering users

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -10,6 +12,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username) 
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -18,3 +26,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
